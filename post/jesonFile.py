ceeafb72bf5d1d64d18bd2b704dd04b4f2859dee
sari huminer <sh6197174@gmail.com>
2019-08-27 00:06:45 +0300

    jeson places

--- post/jesonFile.py
+++ post/jesonFile.py
@@ -13,6 +13,47 @@ def call_google_ocr_api(id_image_path):
     return res
 
 
+<<<<<<< HEAD
+=======
+def getSentenseplace(id_image_path):
+    statment = []
+    response = call_google_ocr_api(id_image_path)
+    # all the fiels togezer
+    res = json.loads(response)
+    # print(res['responses'][0]['textAnnotations'][0]['description'])
+    # statment = response[0]['textAnnotations'][0]['description'].split('\n')
+    # the array that contains all the details mevulgan
+    statment = json.dumps(res['responses'][0]['textAnnotations'][0]['description'])
+    # array that contains the fields from the picture
+    statment = statment.split("\\n")
+    idfieldsplace = {}
+    fields = ['Passpord Card no', 'Nationality', 'Surname', 'Given Names', 'Sex', 'Date of Birth', 'Place of Birth']
+    statment[0] = statment[0][1:]
+    # !!!!!!!!!check spelling for the data that comes from ocr-esti
+    # and organize the word
+    sfield = res['responses'][0]['textAnnotations']
+    sfield = sfield[1:]
+
+    indexOfWord = 1
+    index = 0
+    for s in statment:
+        print(s)
+    print("the values..........................................................")
+    # find the positions in the json filed for every field
+    for s in statment:
+        cnt = s.count(' ') + 1
+        #     if s in fields
+        # if(contains(s)==True) esti function:
+        # idfields[fields[index]]=fieldvalue() elisheva function
+        idfieldsplace[s] = indexOfWord
+        indexOfWord += cnt
+        index += 1
+    for key, val in idfieldsplace.items():
+        print("{} :{}".format(key, val), end="\n")
+
+
+"""
+>>>>>>> place in json
 def getSentense(id_image_path):
     statment = []
     response = call_google_ocr_api(id_image_path)
@@ -28,21 +69,40 @@ def getSentense(id_image_path):
     # !!!!!!!!!check spelling for the data that comes from ocr-esti
     # and organize the word
     sfield = res['responses'][0]['textAnnotations']
+<<<<<<< HEAD
     sfield = sfield[1:]
 
     for s in statment:
+=======
+
+    indexOfWord=0
+    for s in statment:
+        sfield = sfield[1:]
+>>>>>>> place in json
         print(s)
     print("the values..........................................................")
 
     for s in statment:
         # print(s)
         index = 0
+<<<<<<< HEAD
         for f in sfield:
             i = 0
+=======
+        #if containInFields(s):
+        indexOfWord+=
+        for f in sfield:
+            i = 0
+            found = 0
+>>>>>>> place in json
             for c in s:
                 # if c.isalpha():
                 # print(c)
                 if c == ' ':
+<<<<<<< HEAD
+=======
+                    found = 1
+>>>>>>> place in json
                     break
                 if c != f['description'][i]:
                     i = -1
@@ -51,8 +111,15 @@ def getSentense(id_image_path):
             # if s.find(f['description']) == 0:
             if i > 0:
                 print(f['description'])
+<<<<<<< HEAD
         index += 1
 
+=======
+            if found == 1:
+                break
+        index += 1
+        # if dont have the corresponding field or faund it
+>>>>>>> place in json
     #   sfield = [e for i, e in res['responses'][0]['textAnnotations'] if res['responses'][0]['textAnnotations'][e]['description'] == s]
 
     #  for i, index in sfield:
@@ -63,7 +130,14 @@ def getSentense(id_image_path):
     # idfields[s] = '12'
     # for k,v in idfields.items():
     #  print(k+'=>'+v)
+<<<<<<< HEAD
 
 
 if __name__ == "__main__":
     getSentense("C:\\Users\\This_User\\Pictures\\Camera Roll\\WIN_20190813_10_33_43_Pro.jp")
+=======
+"""
+
+if __name__ == "__main__":
+    getSentenseplace("C:\\Users\\This_User\\Pictures\\Camera Roll\\WIN_20190813_10_33_43_Pro.jp")
+>>>>>>> place in json

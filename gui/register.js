d48d00c3f3a6490d2d4c93935ef7912853680d6c
sari huminer <sh6197174@gmail.com>
2019-08-27 15:47:17 +0300

    open camera

---
+++ gui/register.js
@@ -0,0 +1,8 @@
+// var eventc=document.getElementById("camera_btn");
+// eventc.addEventListener('click',opencamera())
+
+function opencamera(){
+    debugger;
+//alert("hellow camera!")
+window.open('cammera.html');
+}

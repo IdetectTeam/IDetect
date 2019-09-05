2105b1d5d94e1597bb0fa0d035c13fe3c3c45ebd
sari huminer <sh6197174@gmail.com>
2019-09-05 14:33:16 +0300

    install ok

--- gui/installation/install.js
+++ gui/installation/install.js
@@ -1,9 +1,10 @@
 
 var index=-1;
-var config_fields=new Object();
+var config_fields=[];
 var passport_usa=new Array("passpord Card no","nationality","surname","given names","gender","date of birth","place of birth");
 var length=passport_usa.length;
 var elm=new Object();
+var dict=[];
 // $('#my-modal').modal('show')
 //               .draggable();ss
 //window.addEventListener("message", receiveMessage, false);
@@ -13,10 +14,19 @@ window.addEventListener('message', receiveMessage, false);
 
 
 function receiveMessage($event) {
+    // alert(event.origin);
        if ($event&& event.origin !== "http://127.0.0.1:5000")
         return;
-     else
-     elm=$event.data;
+     else{
+        elm=$event.data;
+        if(index==0)
+         config_fields.push({
+            key:   "site location",
+            value: event.origin
+        });
+        // config_fields['site location']=event.origin;
+     }
+    
     //  var x = document.createElement("P");
     //         x.id="manual";
     //         var t = document.createTextNode($event.data);
@@ -47,7 +57,10 @@ if(index<passport_usa.length-1){
    index++;
     if(index>-1){
     if("gender"==passport_usa[index])
-    {config_fields[passport_usa[index-1]]=elm;
+    {      config_fields.push({
+        key:   passport_usa[index-1],
+        value: elm
+    });
         Confirm.open({
             title: 'gender',
             message: 'Are you have radio button for gender field?',
@@ -68,7 +81,11 @@ if(index<passport_usa.length-1){
     else
     {
         if(index>0)
-            config_fields[passport_usa[index-1]]=elm;
+            // config_fields[passport_usa[index-1]]=elm;
+             config_fields.push({
+                key:   passport_usa[index-1],
+                value: elm
+            });
             document.getElementById("manual").innerHTML=`click on field ${passport_usa[index]}`;
     }
     
@@ -99,12 +116,35 @@ if(index<passport_usa.length-1){
 
     const Confirm = {
         open (options) {
+            alert("hjhh");
             options = Object.assign({}, {
-                title: '',
-                message: '',
+                title: 'title',
+                message: 'are you sure?',
                 okText: 'OK',
                 cancelText: 'Cancel',
-                onok: function () {},
+                onok: () => {
+                alert('on ok');
+                    
+                    
+                    for(var key in config_fields){
+                        if(key!=male&&key!=female&&key!=site)
+                        dict.push({
+                           key:key,
+                           value:config_fields[key] 
+                        })
+                        else
+                        if(key==male)
+                        dict.push({
+                            key:"jender",  
+                      value:[config_fields[male],config_fields[female]]
+                         })
+                    }
+                    console.log(dict);      
+                    // dict.push({
+                    //     key:config_fields[0].value,
+                    //     value: config_fields
+                    // });
+                },
                 oncancel: function () {}
             }, options);
             
@@ -163,19 +203,21 @@ if(index<passport_usa.length-1){
             });
         }
     };
-    document.getElementById("loadpage").addEventListener('click',function(){
-        Confirm.open({
-            title: 'Background Change',
-            message: 'Are you sure you wish the background color?',
-            onok: () => {
-              //document.body.style.backgroundColor = 'blue';
-            }
-          })   
-    })
+//     document.getElementById("loadpage").addEventListener('click',function(){
+//         Confirm.open({
+//             title: 'Background Change',
+//             message: 'Are you sure you wish the background color?',
+//             onok: () => {
+//               //document.body.style.backgroundColor = 'blue';
+//               alert("ok");
+              
+//             }
+//           })   
+//     })
 
     
     
-}, false);
+ }, false);
 
 
 

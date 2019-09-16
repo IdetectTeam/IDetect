ee724c348be0f9e6a43c72ff5d424fa6a88d729a
ruth-zon <ruthzon711@gmail.com>
2019-09-16 09:08:48 +0300

    add style to install

--- gui/installation/install.js
+++ gui/installation/install.js
@@ -9,6 +9,7 @@ var dict = {};
 var sex = {};
 var json_response;
 var originsite;
+var about=0;
 
 
 window.addEventListener('message', receiveMessage, false);
@@ -26,6 +27,16 @@ function receiveMessage($event) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("aboutButton").addEventListener('click',function () {
+        about++;
+        if (about %2== 1) {
+            document.getElementById("about").style.display = "inline";
+        }
+        else {
+            document.getElementById("about").style.display = "none";
+
+        }
+    })
     $("#myModal").modal('show');
     document.getElementById("manual").innerHTML = "please follow the instructions!";
     index = -1;
@@ -65,19 +76,7 @@ document.addEventListener('DOMContentLoaded', function () {
                         }
 
                     })
-                    // Confirm.open({
-                    //     title: 'gender',
-                    //     message: 'Are you have radio button for gender field?',
-                    //     onok: () => {
-                    //         passport_usa.splice(index, 1, "male", "female");
-                    //         document.getElementById("manual").innerHTML = `click on field <b>${passport_usa[index]}</b>`;
 
-                    //     },
-                    //     oncancel: () => {
-                    //         document.getElementById("manual").innerHTML = `click on field <b>${passport_usa[index]}</b>`;
-                    //     }
-
-                    // })
                 }
                 else {
                     if (passport_usa[index - 1] == 'male' || passport_usa[index - 1] == 'female') {
@@ -111,104 +110,6 @@ document.addEventListener('DOMContentLoaded', function () {
     })
 
 
-
-    // const Confirm = {
-
-    //     open(options) {
-    //         Swal.fire({
-    //             title: 'Do you want to save?',
-    //             text: "You won't be able to revert this!",
-    //             type: 'question',
-    //             showCancelButton: true,
-    //             confirmButtonColor: '#3085d6',
-    //             cancelButtonColor: '#d33',
-    //             cancelText: 'Cancel',
-    //             confirmButtonText: 'Save'
-    //         }).then((result) => {
-    //             if (result.value) {
-    //                 options.onok();
-    //                 Swal.fire(
-    //                     // 'The configuration has been saved',
-    //                     'The configuration has been saved.',
-    //                     'success'
-    //                 );
-    //             }
-    //             else {
-    //                 options.oncancel();
-    //             }
-
-    //         })
-    // options = Object.assign({}, {
-    //     title: '',
-    //     message: '',
-    //     okText: 'OK',
-    //     cancelText: 'Cancel',
-    //     onok: function () {
-
-
-    //         // dict.push({
-    //         //     key:config_fields[0].value,
-    //         //     value: config_fields
-    //         // });
-    //     },
-    //     oncancel: function () { }
-    // }, options);
-
-    // const html = `
-    //     <div class="confirm">
-    //         <div class="confirm__window">
-    //             <div class="confirm__titlebar">
-    //                 <span class="confirm__title">${options.title}</span>
-    //                 <button class="confirm__close">&times;</button>
-    //             </div>
-    //             <div class="confirm__content">${options.message}</div>
-    //             <div class="confirm__buttons">
-    //                 <button class="confirm__button confirm__button--ok confirm__button--fill">${options.okText}</button>
-    //                 <button class="confirm__button confirm__button--cancel">${options.cancelText}</button>
-    //             </div>
-    //         </div>
-    //     </div>
-    // `;
-
-    // const template = document.createElement('template');
-    // template.innerHTML = html;
-
-    // Elements
-    //     const confirmEl = template.content.querySelector('.confirm');
-    //     const btnClose = template.content.querySelector('.confirm__close');
-    //     const btnOk = template.content.querySelector('.confirm__button--ok');
-    //     const btnCancel = template.content.querySelector('.confirm__button--cancel');
-
-    //     confirmEl.addEventListener('click', e => {
-    //         if (e.target === confirmEl) {
-    //             options.oncancel();
-    //             this._close(confirmEl);
-    //         }
-    //     });
-
-    //     btnOk.addEventListener('click', () => {
-    //         options.onok();
-    //         this._close(confirmEl);
-    //     });
-
-    //     [btnCancel, btnClose].forEach(el => {
-    //         el.addEventListener('click', () => {
-    //             options.oncancel();
-    //             this._close(confirmEl);
-    //         });
-    //     });
-
-    //     document.getElementById('myModal').appendChild(template.content);
-    //     },
-
-    //     _close(confirmEl) {
-    //         confirmEl.classList.add('confirm--close');
-
-    //         confirmEl.addEventListener('animationend', () => {
-    //             document.getElementById('myModal').removeChild(confirmEl);
-    //         });
-    //     }
-    // };
     document.getElementById("loadpage").addEventListener('click', function () {
         Swal.fire({
             title: 'Configuration end,',
